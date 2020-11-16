e6632a89edba79246aeec2d2cf9edef27de98b72
s62195541 <baranoral21@hotmail.com>
2020-11-16 09:42:27 +0000

    example4

--- lab5/example4.py
+++ lab5/example4.py
@@ -2,9 +2,10 @@ password = 'abc123'
 
 guess = input('Enter a password!')
 
-if guess=='help':
-  print(password[0])
-elif guess==password:
-  print('Welcome')
-else:
-  print('Wrong')
+while True:
+  if guess=='help':
+    print(password[0])
+  elif guess==password:
+    print('Welcome')
+  else:
+    print('Wrong')
