ab156cfb2e62f56d17c17a9463c2f09c2c31dbc6
s62195541 <baranoral21@hotmail.com>
2020-11-16 08:59:15 +0000

    example1

---
+++ lab3/example5.py
@@ -0,0 +1,11 @@
+day = int(input('Enter a day::'))
+month = input('Enter a month')
+
+if month=="march" or month=="pril" or month=="may" or month=="june":
+  if month=="march" and day>=20 or month=="june"`and day<21:
+    print('Spring')
+  elif month=='march' and day<20 or :
+    print('Winter')
+  elif month=='june' and day>=21 or month=='july' or month=='agust' or month=='september' and day<22:
+    print('Summer')
+  
---
+++ lab5/example1.py
@@ -0,0 +1,5 @@
+number = int(input('Enter a number!'))
+i = 1
+while i==9:
+  print(number , 'x'  , i "="  , i^number) 
+  i = i + 1
