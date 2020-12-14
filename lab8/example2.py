731efdb85a55b4f7ba9fb2147e2dec32bbe67d61
s62195541 <baranoral21@hotmail.com>
2020-12-14 08:55:07 +0000

    example2 lab8

---
+++ lab8/example2.py
@@ -0,0 +1,24 @@
+def is_prime(n):
+  if n>1:
+    for i in range(2,n+1):
+      if n%i!=0:
+        return True
+      else:
+        return False
+  else:
+    return False
+
+def print_primes_between(n1,n2):
+  for i in range(n1,n2):
+   if is_prime(i):
+    print(i)
+
+def main():
+  n1=int(input('Enter a number'))
+  is_prime(n1)
+  
+  n2=int(input('Enter a number'))
+  is_prime(2)
+
+  print_primes_between(n1,n2)
+main()
